98e4c7a69d47c64defb7d5646c87d1cea9e71073
marcio <marciot2@gmail.com>
2025-09-09 23:58:24 -0300

    rrrr

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,4 @@
 export const BACKEND = process.env.REACT_APP_API_URL;
-export const FRONTEND = process.env.REACT_APP_WEB_URL;
+export const FRONTEND = process.env.REACT_APP_WEB_URL || "http://localhost:3000";
+
 
